3fe7630de873c5b12c56b383c497f0e373270b0f
Seu Nome <seu_email@example.com>
2025-04-13 17:29:44 -0300

    oi adicionado

---
+++ src/main/java/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,9 @@
+package one.digitalinnovation.basecamp;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculadora.soma(10, 5);
+        Calculadora.subtracao(10, 5);
+        Calculadora.multiplicacao(10, 5);
+        Calculadora.divisao(10, 5);
+    }
